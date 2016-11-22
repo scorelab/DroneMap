4f3a2a3e23ad28e23c8f0f273235fb27e5889bd7
ඉමාල් තියුනුවන්(Imal Thiunuwan) <ithiunuwan@gmail.com>
2016-11-22 06:12:32 -0500

    added confi.py file

---
+++ config.py
@@ -0,0 +1,14 @@
+import ConfigParser as cp
+import json
+
+config = cp.RawConfigParser()
+config.read('/config/config.cfg')
+
+drone_id = json.loads(config.get("drone","d_id"))
+simulation = config.getboolean("drone", "simulation")
+grounded = config.getboolean("drone", "grounded")
+connection_list = json.loads(config.get("drone", "connection_list"))
+
+
+
+
