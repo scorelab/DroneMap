193e4b9af3e667d5d15e7b5acaa2eb82973f8b87
Imal1992 <ithiunuwan@gmail.com>
2016-11-30 11:44:56 +0530

    Clear class errors

--- db_connection/db_conn.py
+++ db_connection/db_conn.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Created by Imal thiunuwan using Intellij Idea
 
 import pymongo
@@ -8,15 +7,4 @@ client = pymongo.MongoClient("link to the remote database/dbname") # defaults to
 db = client.db_name
 
 # print the number of documents in a collection
-=======
-#Created by Imal thiunuwan using Intellij Idea
-
-import pymongo
-
-client = pymongo.MongoClient("link to the remote database/dbname") # defaults to port 27017
-
-db = client.db_name
-
-# print the number of documents in a collection
->>>>>>> 26846c361adaea2e3e36788f1b50de57f6e1e958
 print db.collection_name.count()
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Created by Imal thiunuwan using Intellij Idea
 
 import unittest
@@ -30,37 +29,4 @@ class TestDrone:
     def main(self):
         print self.testid
         print self.test_simulation
-=======
-#Created by Imal thiunuwan using Intellij Idea
-
-import unittest
-
-class TestDrone:
-
-    def test_id(self,id):
-        if self.id in id:
-            print("ID already exists")
-
-        else:
-            print("ID successfully added")
-
-    def test_simulation(self,simulation,grounded):
-        if self.simulation == True:
-            print("simulation is OK")
-
-        elif (self.grounded == True):
-                print("")
-
-        else:
-            print("")
-
-    def check_connection_list(connection_list):
-        for i in connection_list:
-            print i
-
-
-    def main(self):
-        print self.testid
-        print self.test_simulation
->>>>>>> 26846c361adaea2e3e36788f1b50de57f6e1e958
         print self.check_connection_list
